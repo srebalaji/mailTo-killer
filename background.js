d55b2cd7976e1b124a1bf451c5cb673b159f59b7
srebalaji <srebalaji@outlook.com>
2016-11-14 23:19:24 +0530

    removing background.js

--- background.js
+++
@@ -1,7 +0,0 @@
-    var link = document.getElementById('qwer');
-    console.log(link);
-    // onClick's logic below:
-    link.addEventListener('click', function() {
-        alert("yell..!");
-        console.log("yehh");
-    });
